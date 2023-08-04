8827330c794c493b793b932f235a4ff624df05bc
tunahanozen <tunaz783@gmail.com>
2023-08-04 12:32:33 +0300

    Development of Admin and UI Infrastructure
    Admin ve ui kısımlarının modül ve layoutlarının temeli oluşturuldu.Admin ve ui için temel modül ve componentlerde oluşturuldu.

---
+++ ETicaretClient/src/app/admin/components/components.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { CustomersModule } from './customers/customers.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    OrdersModule,
+    ProductsModule,
+    CustomersModule,
+    DashboardModule
+  ],
+
+})
+export class ComponentsModule { }
--- ETicaretClient/src/app/app.module.ts
+++ ETicaretClient/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,AdminModule,UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
