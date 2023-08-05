0fdf226b58758f71da1c84741b8fd4869772f328
tunahanozen <tunaz783@gmail.com>
2023-08-05 14:47:50 +0300

    Angular Material
    Angular material kütüphanesi eklendi.Admin layout ve side bar kısmı düzenlendi.

--- ETicaretClient/src/app/app.module.ts
+++ ETicaretClient/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -13,7 +14,7 @@ import { UiModule } from './ui/ui.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,AdminModule,UiModule
+    AppRoutingModule,AdminModule,UiModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
